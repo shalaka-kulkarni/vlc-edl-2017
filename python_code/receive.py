5784972fa928fea1b1acc537e369ba302fa73632
saurabhpinjani <saurabh.29pinjani@gmail.com>
2017-04-26 18:08:17 +0530

    Commits added again

--- python_code/receive.py
+++ python_code/receive.py
@@ -2,43 +2,41 @@ import serial
 import sys
 from time import time
 
-conn = serial.Serial('COM5',115200,timeout=0.01)
+conn = serial.Serial('COM3',115200,timeout=0.01)
 print("We have a connection")
 
-with open('received_data.txt', "w") as outfile:
-  data = ""
+while(1):
+  data = []
   while(1):
-    r = conn.read(1)
+    r = conn.read(1) #reading data from the com  port
     if(r!=b'1' and r!=b'0' and r!=b''):
       break
     if(r!=b''):
-      print(r)
-      data = data+r.decode("utf-8")
       
-  print(data)
-  outfile.write(data)
-  outfile.close()
+      data.append(r) #adding bits received into a list
+      
 
-bit_per_sym =1
-char_size =int(8/bit_per_sym)
+  data =[int(x) for x in data] # converting bits to int
+
+ 
+  
 
 
-bit_per_sym =1
-char_size =int(8/bit_per_sym)
-
-with open('received_data.txt') as f:
-	array = []
-	for line in f:
-		for i in range(0,int(len(line)/char_size)):
-			encoded_char =line[i*char_size:(i+1)*char_size]
-			ascii_val=0
-			for j in encoded_char:
-				ascii_val= (ascii_val * (2**bit_per_sym))+int(j)
-			array.append(chr(ascii_val))
-
-with open('reverse_parse_file.txt', "w") as output:
-	for i in array:
-		output.write(i)			
+  char_size=8; # each character is represented by 8 bits
+  array=""                
+  for i in range(0,int(len(data)/char_size)):
+    encoded_char =data[i*char_size:(i+1)*char_size] # picking out 8 bits that represent a character
+    ascii_val=0
+    for j in encoded_char:
+      ascii_val= (ascii_val * (2))+int(j)# finding the ascii value of the character from the bits.
+    array=array+(chr(ascii_val)) # converting ascii code to characters and adding to the string
+  print("                             "+array)
+  print("")
+  print("")
+  print("")
+  print("")
+  print("")
+		
 		
 
   
